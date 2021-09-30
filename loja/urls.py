992181662de0555cf7fd337769713f2db7e9f7e6
bruno <bruno.teixeira@dafiti.com.br>
2021-09-30 14:42:58 -0300

    Making connections with template, static and urls

---
+++ loja/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from loja import views
+
+urlpatterns = [
+    path('', views.productsList, name='productsList'),
+    path('product/<int:pk>/', views.productDetail, name='productDetail'),
+]
--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from loja.models import Product
 
 # Create your views here.
+
+def productsList(request):
+
+    products = Product.objects.filter()
+    return render(request, 'loja/productList.html', {'products': products})
+
+
+def productDetail(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'loja/productDetail.html', {'product': product})
