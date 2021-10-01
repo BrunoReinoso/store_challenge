5c4cc1ed4ff487787cdeeb4b654499b72b3aaebd
bruno <bruno.teixeira@dafiti.com.br>
2021-10-01 14:10:53 -0300

    Trying to show image

--- loja/models.py
+++ loja/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     productType = models.CharField(max_length=50)
     productBrand = models.CharField(max_length=50)
     productColor = models.CharField(max_length=50)
-    productSize = models.CharField(max_length=3)  
-    productDescription = models.CharField
+    productSize = models.CharField(max_length=3)
+    productDescription = models.CharField(max_length=100)
     productPrice = models.DecimalField(max_digits=5, decimal_places=2, default = 1.99)
-    productImage = models.ImageField(upload_to='loja/productImages/')
+    productImage = models.ImageField(upload_to='loja/')
