4de56f2b01f53b0d6d0194a1e79eecf370dfa4b8
bruno <bruno.teixeira@dafiti.com.br>
2021-10-01 09:52:16 -0300

    Resolving conflicts

--- loja/urls.py
+++ loja/urls.py
@@ -3,12 +3,6 @@ from django.urls import path
 from loja import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.productList, name='productList'),
-    path('product/<int:pk>/', views.productDetail, name='productDetail'),
-]
-=======
     path('', views.productsList, name='productsList'),
     path('product/<int:pk>/', views.productDetail, name='productDetail'),
 ]
->>>>>>> urls
