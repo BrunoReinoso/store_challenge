4c41a71323b6b210fcdecfc6266ac979dccb58ff
bruno <bruno.teixeira@dafiti.com.br>
2021-10-01 16:28:52 -0300

    Resolving problems

--- loja/models.py
+++ loja/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
-    
-    product_type = models.CharField(max_length=50)
-    product_brand = models.CharField(max_length=50)
-    product_color = models.CharField(max_length=50)
-    product_size = models.CharField(max_length=3)
-    product_description = models.CharField(max_length=100)
-    product_price = models.DecimalField(max_digits=5, decimal_places=2, default = 1.99)
-    product_image = models.ImageField(upload_to='loja/')
+    EXTRA_SMALL = 'PP'
+    SMALL = 'P'
+    MEDIUM = 'M'
+    LARGE = 'G'
+    EXTRA_LARGE = 'GG'
+    SIZE_CHOICES = (
+        (EXTRA_SMALL, 'PP'),
+        (SMALL, 'P'),
+        (MEDIUM, 'M'),
+        (LARGE, 'G'),
+        (EXTRA_LARGE, 'GG')
+    )
+    type = models.CharField(_('type'), max_length=50)
+    brand = models.CharField(_('brand'), max_length=50)
+    color = models.CharField(_('color'), max_length=50)
+    size = models.CharField(_('size'), max_length=3, choices=SIZE_CHOICES)
+    description = models.CharField(_('description'), max_length=100)
+    price = models.DecimalField(_('price'), max_digits=5, decimal_places=2, default = 1.99)
+    image = models.ImageField(_('image'), upload_to='loja')
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __str__(self):
+        return f'{self.type}'
