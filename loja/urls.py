1623109ffed8168f658dadc162d319da1cb40474
Adailton Nascimento <dhelbegors@gmail.com>
2021-10-01 15:44:23 -0300

    change url configurations

--- loja/urls.py
+++ loja/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from loja import views
 
 urlpatterns = [
     path('', views.productsList, name='productsList'),
     path('product/<int:pk>/', views.productDetail, name='productDetail'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
