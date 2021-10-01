5063aabf12bb0e4b33c56c6094c374879a963fc5
bruno <bruno.teixeira@dafiti.com.br>
2021-10-01 15:19:33 -0300

    Dadal's help

--- loja/models.py
+++ loja/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Product(models.Model):
     
-    productType = models.CharField(max_length=50)
-    productBrand = models.CharField(max_length=50)
-    productColor = models.CharField(max_length=50)
-    productSize = models.CharField(max_length=3)
-    productDescription = models.CharField(max_length=100)
-    productPrice = models.DecimalField(max_digits=5, decimal_places=2, default = 1.99)
-    productImage = models.ImageField(upload_to='loja/')
+    product_type = models.CharField(max_length=50)
+    product_brand = models.CharField(max_length=50)
+    product_color = models.CharField(max_length=50)
+    product_size = models.CharField(max_length=3)
+    product_description = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=5, decimal_places=2, default = 1.99)
+    product_image = models.ImageField(upload_to='loja/')
