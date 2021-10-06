05c69267ce7a05f72b97892f0c2ee02b0767b1f2
bruno <bruno.teixeira@dafiti.com.br>
2021-10-06 13:49:42 -0300

    Api with based generic class-based views

--- loja/urls.py
+++ loja/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from loja import views
 
-from .views import ProductAPIView
-
+from .views import productList, productDetail, ProductListAPI, ProductDetailAPI
 urlpatterns = [
     path('', views.productList, name='productList'),
     path('product/<int:pk>/', views.productDetail, name='productDetail'),
-    path('productAPI/', ProductAPIView.as_view(), name='productAPI')
+    path('productList/', ProductListAPI.as_view(), name='productListAPI'),
+    path('productDetail/<int:pk>/', ProductDetailAPI.as_view(), name='productDetailAPI')
 ]
--- loja/views.py
+++ loja/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from loja.models import Product
 from loja.serializers import ProductSerializer
 
-from rest_framework.views import APIView , Response
-from rest_framework import status
+from rest_framework import generics
 
 # Create your views here.
 
@@ -18,15 +17,11 @@ def productDetail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'loja/productDetail.html', {'product': product})
 
-class ProductAPIView(APIView):
+class ProductListAPI(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
